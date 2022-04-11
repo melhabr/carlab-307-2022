342d588c2683c65166599aac6039287d0b8edb3c
Mason El-Habr <67023671+melhabr@users.noreply.github.com>
2022-04-11 14:23:15 -0400

    fixed comments

--- control_car.py
+++ control_car.py
@@ -3,8 +3,8 @@ class CarControl:
 
 	def __init__(self):
 		pass
-		// Initialize connection to car
+		# Initialize connection to car
 
 	def send_value(speed, direction):
 		pass
-		// Send control value to car 
+		# Send control value to car 
